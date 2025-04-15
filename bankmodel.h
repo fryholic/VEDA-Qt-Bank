1aaf19c5b836f320b33d3b83718c48b4800a24ba
hosuho <oafsh95@gmail.com>
2025-04-15 15:15:35 +0900

    회원가입 및 계좌추가 업데이트

---
+++ bankmodel.h
@@ -0,0 +1,85 @@
+#ifndef BANKMODEL_H
+#define BANKMODEL_H
+
+#include <QObject>
+#include <QString>
+#include <QList>
+#include <QVariant>
+#include <QMap>
+#include <QSqlDatabase>
+
+class Account {
+public:
+    QString accountNumber;
+    QString accountName;
+    double balance;
+    
+    Account(const QString &number, const QString &name, double initialBalance)
+        : accountNumber(number), accountName(name), balance(initialBalance) {}
+};
+
+class Transaction {
+public:
+    QString date;
+    QString type;
+    double amount;
+    QString description;
+    
+    Transaction(const QString &d, const QString &t, double a, const QString &desc)
+        : date(d), type(t), amount(a), description(desc) {}
+};
+
+class BankModel : public QObject
+{
+    Q_OBJECT
+    
+public:
+    explicit BankModel(QObject *parent = nullptr);
+    ~BankModel();
+    
+    QString userName() const { return m_userName; }
+    void setUserName(const QString &name);
+    
+    double totalBalance() const;
+    
+    QString currentAccountNumber() const { return m_currentAccountNumber; }
+    void setCurrentAccountNumber(const QString &accountNumber);
+    
+    void logout();
+    QVariantList getAccounts() const;
+    QVariantMap getAccountDetails(const QString &accountNumber) const;
+    QVariantList getTransactions(const QString &accountNumber) const;
+    bool deposit(const QString &accountNumber, double amount, const QString &verificationCode);
+    bool withdraw(const QString &accountNumber, double amount, const QString &verificationCode);
+    bool transfer(const QString &fromAccount, const QString &toAccount, double amount, const QString &verificationCode);
+    bool verifyAmount(double amount, const QString &type, const QString &accountNumber = QString());
+
+    // bankmodel.h 안에 public 함수 추가
+    bool registerUser(const QString &username, const QString &password); // 회원가입
+    bool login(const QString &username, const QString &password); // 로그인
+
+    bool createAccount(const QString &accountName, double initialBalance);
+
+signals:
+    void userNameChanged();
+    void totalBalanceChanged();
+    void currentAccountNumberChanged();
+    void transactionCompleted(const QString &type, double amount);
+    
+private:
+    QString m_userName;
+    QString m_currentAccountNumber;
+    QList<Account> m_accounts;
+    QMap<QString, QList<Transaction>> m_transactions;
+    QSqlDatabase db;
+
+    void initializeDatabase();
+    
+    bool isLoggedIn;
+    const double MAX_AMOUNT = 4200000000.0; // 42억
+
+    // private 함수 안에 추가
+    void createUserTable(); // 사용자 테이블 생성
+};
+
+#endif // BANKMODEL_H
---
+++ verificationwidget.cpp
@@ -0,0 +1,86 @@
+#include "verificationwidget.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QIntValidator>
+
+VerificationWidget::VerificationWidget(QWidget *parent) : QWidget(parent)
+{
+    // 상단 헤더
+    QWidget *headerWidget = new QWidget(this);
+    headerWidget->setObjectName("transactionHeader");
+    
+    m_backButton = new QPushButton("뒤로", this);
+    m_backButton->setObjectName("secondaryButton");
+    
+    m_titleLabel = new QLabel("인증번호 입력", this);
+    m_titleLabel->setObjectName("pageTitle");
+    m_titleLabel->setAlignment(Qt::AlignCenter);
+    
+    QHBoxLayout *headerLayout = new QHBoxLayout(headerWidget);
+    headerLayout->addWidget(m_backButton);
+    headerLayout->addWidget(m_titleLabel, 1);
+    headerLayout->addSpacerItem(new QSpacerItem(m_backButton->sizeHint().width(), 0, QSizePolicy::Fixed, QSizePolicy::Minimum));
+    headerLayout->setContentsMargins(20, 20, 20, 20);
+    
+    // 인증번호 입력 영역
+    QWidget *verificationCard = new QWidget(this);
+    verificationCard->setObjectName("inputCard");
+    
+    QLabel *verificationTitle = new QLabel("인증번호 5자리를 입력하세요", this);
+    verificationTitle->setObjectName("inputTitle");
+    
+    m_verificationEdit = new QLineEdit(this);
+    m_verificationEdit->setPlaceholderText("인증번호");
+    m_verificationEdit->setObjectName("verificationInput");
+    m_verificationEdit->setMaxLength(5);
+    
+    // 5자리 숫자만 입력 가능하도록 설정
+    QIntValidator *validator = new QIntValidator(0, 99999, this);
+    m_verificationEdit->setValidator(validator);
+    
+    QLabel *hintLabel = new QLabel("* 테스트용으로 아무 5자리 숫자나 입력하세요", this);
+    hintLabel->setObjectName("hintLabel");
+    
+    QVBoxLayout *verificationCardLayout = new QVBoxLayout(verificationCard);
+    verificationCardLayout->addWidget(verificationTitle);
+    verificationCardLayout->addWidget(m_verificationEdit);
+    verificationCardLayout->addWidget(hintLabel);
+    verificationCardLayout->setSpacing(10);
+    verificationCardLayout->setContentsMargins(16, 16, 16, 16);
+    
+    // 다음 버튼
+    m_nextButton = new QPushButton("다음", this);
+    m_nextButton->setObjectName("primaryButton");
+    m_nextButton->setEnabled(false);
+    
+    // 메인 레이아웃
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(headerWidget);
+    mainLayout->addWidget(verificationCard);
+    mainLayout->addStretch();
+    mainLayout->addWidget(m_nextButton);
+    mainLayout->setContentsMargins(20, 20, 20, 20);
+    
+    // 시그널-슬롯 연결
+    connect(m_backButton, &QPushButton::clicked, this, &VerificationWidget::backRequested);
+    connect(m_nextButton, &QPushButton::clicked, this, &VerificationWidget::onNextButtonClicked);
+    connect(m_verificationEdit, &QLineEdit::textChanged, this, [this](const QString &text) {
+        m_nextButton->setEnabled(text.length() == 5);
+    });
+}
+
+void VerificationWidget::setupForTransaction(const QString &transactionType)
+{
+    m_transactionType = transactionType;
+    m_verificationEdit->clear();
+    m_nextButton->setEnabled(false);
+}
+
+void VerificationWidget::onNextButtonClicked()
+{
+    QString verificationCode = m_verificationEdit->text();
+    
+    if (verificationCode.length() == 5) {
+        emit verificationCompleted(verificationCode);
+    }
+}
